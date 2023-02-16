2be36733ae8e15e06bf176312f964a9ae62f60e5
ClaytonMain <main.clayton@gmail.com>
2023-02-15 20:55:34 -0700

    Solved problem 1

--- problems/problem0001/problem_0001.py
+++ problems/problem0001/problem_0001.py
@@ -1,7 +1,34 @@
 # Project Euler: Problem 1 - Multiples of 3 or 5
+import helper_funcs as hfs
+
+
+TIME_FUNCTION = True
+
+
+def p0001(n1=3, n2=5, maxn=1000):
+    # Basic principle of inclusion and exclusion.
+    mult_sum = sum(range(n1, maxn, n1))
+    mult_sum += sum(range(n2, maxn, n2))
+    mult_sum -= sum(range(n1 * n2, maxn, n1 * n2))
+    return mult_sum
+
 
 def main():
-	pass
+    ans = p0001()
+    print(f'\n*** Answer ***\n> {ans}')
+
+    if TIME_FUNCTION:
+        hfs.time_function('p0001')
+
 
 if __name__ == '__main__':
-	main()
+    main()
+
+# Printed messages: (SPOILERS)
+# *** Answer ***
+# > 233168
+
+# *** Time Stuff ***
+# Executions: 10000
+# Total exec. time: 0.071089s
+# Avg. exec. time: 0.000007s

f9880e6d6c122a27d82fb278760a3970a0e7939e
ClaytonMain <main.clayton@gmail.com>
2023-02-15 20:56:10 -0700

    Start problem 2

---
+++ problems/problem0002/problem_0002.py
@@ -0,0 +1,21 @@
+# Project Euler: Problem 2 - Even Fibonacci numbers
+import helper_funcs as hfs
+
+
+TIME_FUNCTION = False
+
+
+def p0002():
+	pass
+
+
+def main():
+	ans = p0002()
+	print(f'\n*** Answer ***\n> {ans}')
+
+	if TIME_FUNCTION:
+		hfs.time_function('p0002')
+
+
+if __name__ == '__main__':
+	main()

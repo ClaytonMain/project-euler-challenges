2fd07c71d3bce2d539c350e17f24e06bc2ec421c
ClaytonMain <main.clayton@gmail.com>
2023-02-15 21:32:21 -0700

    Generate problem 3

---
+++ problems/problem0003/problem_0003.py
@@ -0,0 +1,23 @@
+# Project Euler: Problem 3 - Largest prime factor
+import helper_funcs as hfs
+
+
+TIME_FUNCTION = False
+
+
+def p0003():
+	pass
+
+
+def main():
+	ans = p0003()
+	print(f'\n*** Answer ***\n{ans}')
+
+	if TIME_FUNCTION:
+		hfs.time_function('p0003')
+
+
+if __name__ == '__main__':
+	main()
+
+# Output (SPOILERS)

e9e7702132211660111d440afcf65742252dda84
ClaytonMain <main.clayton@gmail.com>
2023-02-16 22:03:11 -0700

    Add difficulty rating to generated problem files

--- start_new_project.py
+++ start_new_project.py
@@ -2,15 +2,17 @@ import argparse
 import os
 import requests
 import shutil
+import re
 from bs4 import BeautifulSoup
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('n', type=int, help='The problem number')
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     n = args.n
+
     padded_n = f'{n:04}'
     project_path = f'problems/problem{padded_n}/'
     if os.path.exists(project_path):
@@ -26,9 +28,10 @@ def main():
 
     # I know it seems silly, but we're going to do two requests to get the
     # information needed for each problem. The problem_url request will
-    # allow us to get the problem title, and the minimal_url request will
-    # allow us to get the problem description a smidge easier (which should
-    # be handy for later problems that start using LaTeX).
+    # allow us to get the problem title and the difficulty rating. The
+    # minimal_url request will allow us to get the problem description a
+    # smidge easier (which should be handy for later problems that start
+    # using LaTeX).
     problem_url = f'https://projecteuler.net/problem={n}'
     minimal_url = f'https://projecteuler.net/minimal={n}'
 
@@ -38,13 +41,18 @@ def main():
     # Hoping that the first H2 element will always be the title, but will
     # update the code if that isn't the case in later problems.
     problem_title = problem_soup.find('h2').text
+    tiptext = problem_soup.find('span', {'class': 'tooltiptext_right'}).text
+    diff_rating = re.search(r'Difficulty rating: .*$', tiptext)[0]
 
     r = s.get(minimal_url)
     minimal_soup = BeautifulSoup(r.text, 'html.parser')
 
     with open(f'{project_path}README.md', 'x') as file:
-        file.write(f'# [Problem {n} - {problem_title}]({problem_url})\n\n')
+        file.write(f'# [Problem {n} - {problem_title}]({problem_url})\n')
+        file.write(f'{diff_rating}\n\n')
         file.write('## Prompt\n\n')
+        # This probably won't work very well once the problems start
+        # including LaTeX, so that'll likely need a fix later.
         lines = [f'> {x}' for x in minimal_soup.find_all(True)]
         file.write('\n'.join(lines))
 
@@ -52,17 +60,18 @@ def main():
         file.writelines(
             [
                 f'# Project Euler: Problem {n} - {problem_title}\n',
+                f'# {diff_rating}\n',
                 'import helper_funcs as hfs\n\n\n',
                 'TIME_FUNCTION = False\n\n\n',
                 f'def p{padded_n}():\n',
-                '\tpass\n\n\n',
+                '    pass\n\n\n',
                 'def main():\n',
-                f'\tans = p{padded_n}()\n',
-                "\tprint(f'\\n*** Answer ***\\n{ans}')\n\n",
-                '\tif TIME_FUNCTION:\n',
-                f"\t\thfs.time_function('p{padded_n}')\n\n\n",
+                f'    ans = p{padded_n}()\n',
+                "    print(f'\\n*** Answer ***\\n{ans}')\n\n",
+                '    if TIME_FUNCTION:\n',
+                f"        hfs.time_function('p{padded_n}')\n\n\n",
                 "if __name__ == '__main__':\n",
-                '\tmain()\n\n',
+                '    main()\n\n',
                 '# Output (SPOILERS)',
             ]
         )

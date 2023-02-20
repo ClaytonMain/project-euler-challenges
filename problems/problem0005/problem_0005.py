9c4148b891cdaf41b60d00bad41de73030c16044
ClaytonMain <main.clayton@gmail.com>
2023-02-20 12:24:51 -0700

    Start project 5

---
+++ problems/problem0005/problem_0005.py
@@ -0,0 +1,19 @@
+# Project Euler: Problem 5 - Smallest multiple
+# Difficulty rating: 5%
+from helperfuncs.helper_funcs import time_decorator
+
+
+@time_decorator()
+def p0005():
+    pass
+
+
+def main():
+    ans = p0005()
+    print(f'\n*** Answer ***\n{ans}')
+
+
+if __name__ == '__main__':
+    main()
+
+# Output (SPOILERS)
